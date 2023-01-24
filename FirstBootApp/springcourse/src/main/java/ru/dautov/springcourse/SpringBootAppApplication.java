637a7f4f1d196c0567c2b39637cae1bf3aaf013d
Dautov Evgeniy <evg.dautov@mail.ru>
2023-01-24 13:06:53 +0300

    Исправление опечаток при создании

--- FirstBootApp/springcourse/src/main/java/ru/dautov/springcourse/SpringBootAppApplication.java
+++ FirstBootApp/springcourse/src/main/java/ru/dautov/springcourse/SpringBootAppApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SprinBootAppApplication {
+public class SpringBootAppApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SprinBootAppApplication.class, args);
+		SpringApplication.run(SpringBootAppApplication.class, args);
 	}
 
 }
--- FirstBootApp/springcourse/src/test/java/ru/dautov/springcourse/SpringBootAppApplicationTests.java
+++ FirstBootApp/springcourse/src/test/java/ru/dautov/springcourse/SpringBootAppApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class SprinBootAppApplicationTests {
+class SpringBootAppApplicationTests {
 
 	@Test
 	void contextLoads() {
