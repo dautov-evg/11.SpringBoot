0f9a09e0109e2127766b73fb976cf75728caf474
Dautov Evgeniy <evg.dautov@mail.ru>
2023-01-24 12:41:27 +0300

    Создание репозитория

---
+++ FirstBootApp/springcourse/src/main/java/ru/dautov/springcourse/SprinBootAppApplication.java
@@ -0,0 +1,13 @@
+package ru.dautov.springcourse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SprinBootAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SprinBootAppApplication.class, args);
+	}
+
+}
---
+++ FirstBootApp/springcourse/src/test/java/ru/dautov/springcourse/SprinBootAppApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.dautov.springcourse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SprinBootAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
